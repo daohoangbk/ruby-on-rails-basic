62af3c9bc8a5a5397e3ef34201c8f73528dd00dd
Dao Cong Hoang <dao_cong_hoang@sekisho.co.jp>
2024-05-01 15:38:15 +0900

    新規：新聞変更画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_28_144941) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_151600) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_articles_on_deleted_at"
   end
 
 end
