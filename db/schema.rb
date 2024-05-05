f89968d01da290394cd902c2ac504b0fbebd521d
daohoang <daohoangbk134@gmail.com>
2024-05-05 18:37:29 +0900

    更新：concern利用

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_064706) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_151722) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "status"
     t.index ["deleted_at"], name: "index_articles_on_deleted_at"
   end
 
@@ -26,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_064706) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
